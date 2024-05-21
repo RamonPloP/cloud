a41f2c5e6c512ded54d4d8528efbce9a1c74c39d
RamonPloP <aspireandlive@gmail.com>
2024-05-20 21:43:24 -0700

    Styles changes

--- index.php
+++ index.php
@@ -115,7 +115,17 @@
     </div>
 </div>
 
-<a href="#" class="download-btn">Descargas</a>
+<button class="download-btn">
+    <a href="descargas.php">
+    <div class="docs"> <h2>Descargas</h2> </div>
+    <div class="download">
+        <svg class="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="currentColor" height="24" width="24" viewBox="0 0 24 24">
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+            <polyline points="7 10 12 15 17 10"></polyline>
+            <line y2="3" x2="12" y1="15" x1="12"></line>
+        </svg>
+  </div>
+</button>
 
 </body>
 </html>
